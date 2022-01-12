d90e659d5d677b32b395eada4e620781ef8adc48
Amy Mai <My.Mai@metropolia.fi>
2022-01-12 10:59:59 +0200

    finished ajax task b

--- components/List.js
+++ components/List.js
@@ -1,20 +1,25 @@
 import { React, useState, useEffect } from "react";
 import { FlatList } from "react-native";
+import { baseUrl } from "../utils/variables";
 import ListItem from "./ListItem";
-
-const url =
-  "https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json";
 
 const List = () => {
   const [mediaArray, setMediaArray] = useState([]);
   const loadMedia = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(baseUrl + "media/");
       if (!response.ok) {
         throw Error(response.statusText);
       }
       const json = await response.json();
-      setMediaArray(json);
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const response = await fetch(baseUrl + "media/" + item.file_id);
+          const mediaData = await response.json();
+          return mediaData;
+        })
+      );
+      setMediaArray(media);
       console.log(mediaArray);
     } catch (error) {
       console.error(error);
@@ -26,7 +31,7 @@ const List = () => {
   return (
     <FlatList
       data={mediaArray}
-      keyExtractor={(item) => item.title}
+      keyExtractor={(item) => item.file_id.toString()}
       renderItem={({ item }) => <ListItem singleMedia={item} />}
     />
   );
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,42 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, Image, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import PropTypes from "prop-types";
+import { uploadsUrl } from "../utils/variables";
 
 const ListItem = (props) => {
   return (
-    <TouchableOpacity style={styles.flexbox}>
-      <Image
-        style={{ margin: 10, width: "50%", height: 300 }}
-        source={{ uri: props.singleMedia.thumbnails.w160 }}
-      />
-      <View
-        style={{
-          width: "45%",
-          alignSelf: "center",
-        }}
-      >
-        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-          {props.singleMedia.title}
-        </Text>
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.imagebox}>
+        <Image
+          source={{ uri: uploadsUrl + props.singleMedia.thumbnails.w160 }}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.textbox}>
+        <Text style={styles.listTitle}>{props.singleMedia.title}</Text>
         <Text>{props.singleMedia.description}</Text>
       </View>
     </TouchableOpacity>
   );
 };
-
 const styles = StyleSheet.create({
-  flexbox: {
+  row: {
     flexDirection: "row",
-    width: "100%",
-    margin: 0,
-    alignContent: "center",
-    backgroundColor: "#d9d9d9",
+    padding: 15,
+    backgroundColor: "#eee",
+    borderRadius: 6,
+    marginHorizontal: 10,
     marginBottom: 5,
+  },
+  imagebox: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    borderRadius: 6,
+  },
+  textbox: {
+    flex: 2,
+    padding: 10,
+  },
+  listTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingBottom: 15,
   },
 });
 
 ListItem.propTypes = {
-  singleMedia: PropTypes.object,
+  singleMedia: PropTypes.object.isRequired,
 };
 
 export default ListItem;
